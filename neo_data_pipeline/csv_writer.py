007dc9eb84d14adb76c888a613a1f59d83cb6e22
Felipe Benigno <felipe.benigno@greenant.com.br>
2024-06-01 13:56:37 -0300

    Feature: add documentation

--- neo_data_pipeline/csv_writer.py
+++ neo_data_pipeline/csv_writer.py
@@ -2,8 +2,22 @@ import csv
 
 
 class CsvWriter:
+    """
+    A utility class for writing data to a CSV file.
+
+    This class provides methods to save data to a CSV file with specified fieldnames.
+    """
+
     @staticmethod
     def save_to_csv(data, fieldnames, filename="neo_data.csv"):
+        """
+        Save data to a CSV file with specified fieldnames.
+
+        Args:
+            data (iterable): An iterable (e.g., list, generator) of dictionaries containing the data to be written.
+            fieldnames (list of str): A list of strings representing the header names for the CSV file.
+            filename (str, optional): The name of the file to which the data will be written. Defaults to "neo_data.csv".
+        """
         with open(filename, "w", newline="", encoding="utf-8") as f:
             dict_writer = csv.DictWriter(f, fieldnames=fieldnames)
             dict_writer.writeheader()
--- neo_data_pipeline/pipeline.py
+++ neo_data_pipeline/pipeline.py
@@ -6,7 +6,26 @@ from neo_data_pipeline.processor import Processor
 
 
 class DataPipeline:
+    """
+    A data pipeline for fetching, processing, and saving Near Earth Object (NEO) data.
+
+    This class orchestrates the fetching of NEO data from NASA's API, processes the data,
+    and saves the results to a CSV file.
+
+    Attributes:
+        api_client (NasaNeoApiClient): The client for accessing NASA's NEO API.
+        data_processor (Processor): The processor for handling and transforming the NEO data.
+        csv_writer (CsvWriter): The writer for saving data to a CSV file.
+        fieldnames (list of str): The list of field names for the CSV file.
+    """
+
     def __init__(self, api_key):
+        """
+        Initialize the DataPipeline with the NASA API key.
+
+        Args:
+            api_key (str): The NASA API key.
+        """
         self.api_client = NasaNeoApiClient(api_key)
         self.data_processor = Processor()
         self.csv_writer = CsvWriter()
@@ -24,6 +43,14 @@ class DataPipeline:
         ]
 
     def run(self, start_date, end_date, output_filename="neo_data.csv"):
+        """
+        Runs the data pipeline to fetch, process, and save NEO data.
+
+        Args:
+            start_date (str): The start date in YYYY-MM-DD format for fetching NEO data.
+            end_date (str): The end date in YYYY-MM-DD format for fetching NEO data.
+            output_filename (str, optional): The name of the file to which the processed data will be saved. Defaults to "neo_data.csv".
+        """
         try:
             neo_data = self.api_client.fetch_neo_data(start_date, end_date)
             processed_data = self.data_processor.process(neo_data, self.api_client)
